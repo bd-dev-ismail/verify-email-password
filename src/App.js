066162bd0709c70cb27c58fd30489b9321b30fa2
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-15 13:10:09 +0600

    middle point of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import SignInForm from './components/SignInForm';
+import SignUpForm from './components/SignUpForm';
+import Main from './layout/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <Main></Main>, children: [
+      {
+        path: '/', element: <SignUpForm></SignUpForm>
+      },
+      {
+        path: '/signUp', element: <SignUpForm></SignUpForm>
+      },
+      {
+        path: '/login', element: <SignInForm></SignInForm>
+      }
+
+    ]
+  }
+])
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqF1FfDd9g-kuFHSaN_CZHpaDHqVAWYZM",
+  authDomain: "verify-email-password.firebaseapp.com",
+  projectId: "verify-email-password",
+  storageBucket: "verify-email-password.appspot.com",
+  messagingSenderId: "410698656165",
+  appId: "1:410698656165:web:d3438c80899bcc2c62604c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
